44f8a1cd984b5909b355f2257fa8b5b597e75e98
Amit Paul Renikuntla <aprenikuntla@gmail.com>
2017-02-06 12:24:38 -0500

    added files

---
+++ company-template.php
@@ -0,0 +1,31 @@
+<?php
+/*
+    Template Name: Company Layout
+*/
+ ?>
+
+<?php get_header(); ?>
+
+<div class="container content">
+		<div class="main block">
+			<?php if(have_posts()) : ?>
+				<?php while(have_posts()) : the_post(); ?>
+					<article class="page">
+					  <h2><?php the_title(); ?></h2>
+            <p class="phone">Call Me: 508-596-4750</p>
+            <?php the_content(); ?>
+					</article>
+				<?php endwhile; ?>
+			<?php else : ?>
+				<?php echo apautop('Sorry, no posts were found'); ?>
+			<?php endif; ?>
+		</div>
+
+    <div class="side">
+			<?php if(is_active_sidebar('sidebar')) : ?>
+				<?php dynamic_sidebar('sidebar'); ?>
+			<?php endif; ?>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
